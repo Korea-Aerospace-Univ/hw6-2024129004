370ea03fdd3d2dd8c7e1cb3a5e88938cfb54d226
2024129004 <0325won@gmail.com>
2026-05-27 16:42:17 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -3,18 +3,20 @@
 int main()
 {
     char arr[11]={};
-    char *p=nullptr, *q=nullptr;
+    char *p, *q;
     
-    int count=0, max=0;
+    int count, max=0;
     char result;
     
     scanf("%s", arr);
 
     for (p=arr; p<arr+10; p++) {
+        count=0;
         for (q=arr; q<arr+10; q++) {
             if (*p==*q) {
                 count++;
             }
+        }
         if (count>max) {
             max=count;
             result=*p;
